cb2ded900799990a4e3d1c72cc051564d45aafa8
Pedro Corpas Cuesta <pedro.corpas@bosonit.com>
2022-04-18 08:52:48 +0200

    Inicialización repositorio

---
+++ src/main/java/com/bosonit/virtualtravel/VirtualTravelApplication.java
@@ -0,0 +1,13 @@
+package com.bosonit.virtualtravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualTravelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualTravelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bosonit/virtualtravel/VirtualTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosonit.virtualtravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualTravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
